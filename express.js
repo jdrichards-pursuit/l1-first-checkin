5e9fa5f4d8699fc9c774d1828d0c975f4ce3be43
jd richards <jdrichardstech@gmail.com>
2024-10-11 12:35:34 -0400

    add import

--- express.js
+++ express.js
@@ -6,11 +6,13 @@ Use line numbers and technical terms.
 const express = require('express')
 const bookmarks = express.Router()
 
+const {getBookmark} = require('./controllers/bookmarks')
+
 // Get Bookmark By Id
 bookmarks.get('/:id', async (req, res) => {
   const { id } = req.params
   const bookmark = await getBookmark(id)
-  if (!bookmark.message) {
+  if (bookmark.id) {
     res.status(200).json(bookmark)
   } else {
     res.status(404).json({ error: 'not found' })
